179337828d15582f4d5d20df6a871b1175162dab
sandhyas30 <sandhya.s6215@gmail.com>
2025-10-24 08:48:58 +0530

    Add multiple PLSQL scripts for various calculations

--- Week1/Day5_PLSQL Basic and Advanced/PLSQL_Queries.sql
+++ Week1/Day5_PLSQL Basic and Advanced/PLSQL_Queries.sql
@@ -1,1 +1,102 @@
+--1. CALCULATE SIMPLE INTEREST
 
+DECLARE
+p NUMBER := 5000; -- principal
+r NUMBER := 4; -- rate of interest
+t NUMBER := 3; -- time in years
+si NUMBER;
+BEGIN
+si := (p * r * t) / 100;
+DBMS_OUTPUT.PUT_LINE('Simple Interest = ' || si);
+END;
+/
+
+
+--2. EMPLOYEE BONUS CALCULATION
+
+DECLARE
+emp_name VARCHAR2(50);
+salary NUMBER;
+bonus NUMBER;
+BEGIN
+emp_name := 'Ananya';
+salary := 52000;
+
+IF salary > 50000 THEN
+bonus := 0.10 * salary;
+ELSE
+bonus := 0.15 * salary;
+END IF;
+
+DBMS_OUTPUT.PUT_LINE('Employee Name: ' || emp_name);
+DBMS_OUTPUT.PUT_LINE('Salary: ' || salary);
+DBMS_OUTPUT.PUT_LINE('Bonus: ' || bonus);
+END;
+/
+
+--3. RETRIEVE AND DISPLAY DEPARTMENT DETAILS
+
+DECLARE
+v_dept DEPT%ROWTYPE;
+v_dept_id DEPT.DEPTNO %TYPE := 10; -- sample dept ID
+BEGIN
+SELECT * INTO v_dept
+FROM DEPT
+WHERE DEPTNO = v_dept_id;
+
+DBMS_OUTPUT.PUT_LINE('Department Name: ' || v_dept.DNAME);
+DBMS_OUTPUT.PUT_LINE('Location ID: ' || v_dept.LOC);
+END;
+/
+
+--4. STUDENT MARKS MANAGEMENT
+
+DECLARE
+TYPE marks_table IS TABLE OF NUMBER INDEX BY VARCHAR2(50);
+student_marks marks_table;
+
+v_name VARCHAR2(50);
+total NUMBER := 0;
+count_students NUMBER := 0;
+avg_marks NUMBER;
+BEGIN
+student_marks('Harsha') := 90;
+student_marks('Sanjana') := 80;
+student_marks('Sandhya') := 95;
+
+v_name := student_marks.FIRST;
+
+WHILE v_name IS NOT NULL LOOP
+DBMS_OUTPUT.PUT_LINE(v_name || ' scored ' || student_marks(v_name));
+total := total + student_marks(v_name);
+count_students := count_students + 1;
+v_name := student_marks.NEXT(v_name);
+END LOOP;
+
+avg_marks := total / count_students;
+DBMS_OUTPUT.PUT_LINE('Total Marks = ' || total);
+DBMS_OUTPUT.PUT_LINE('Average Marks = ' || avg_marks);
+END;
+/
+
+--5. COUNTRY CAPITAL LOOKUP 
+
+DECLARE
+TYPE country_capital_table IS TABLE OF VARCHAR2(50) INDEX BY VARCHAR2(50);
+country_capitals country_capital_table;
+
+v_country VARCHAR2(50);
+BEGIN
+country_capitals('India') := 'New Delhi';
+country_capitals('Japan') := 'Tokyo';
+country_capitals('France') := 'Paris';
+
+v_country := '&country';
+
+IF country_capitals.EXISTS(v_country) THEN
+DBMS_OUTPUT.PUT_LINE('Capital of ' || v_country || ' is ' || country_capitals(v_country));
+ELSE
+DBMS_OUTPUT.PUT_LINE('Country not found.');
+END IF;
+END;
+/

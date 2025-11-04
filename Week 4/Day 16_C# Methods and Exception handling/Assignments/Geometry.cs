f939169280366c2c12ab586a34cc8af53d7dfc35
sandhyas30 <sandhya.s6215@gmail.com>
2025-11-04 15:30:20 +0530

    Implement Geometry class with area calculations

---
+++ Week 4/Day 16_C# Methods and Exception handling/Assignments/Geometry.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace Assignment
+{
+    internal class Program
+    {
+        static int CalculateArea(int side)
+        {
+            return side * side;
+        }
+
+        static double CalculateArea( double r)
+        {
+            return Math.PI * r * r;
+        }
+
+        static float CalculateArea(float length, float breadth)
+        {
+            return length * breadth;
+        }
+    
+   
+        static void Main(string[] args)
+        {
+        
+            Console.WriteLine("Area of square:"+ CalculateArea(3));
+        
+            Console.WriteLine("Area of Circle:"+CalculateArea(4.5));
+            Console.WriteLine("Area of Rectangle:"+ CalculateArea(30,40));
+
+        }
+    }
+}
